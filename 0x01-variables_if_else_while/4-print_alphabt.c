503f613f2ca3801524e25037b77c166c7e3f2df1
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-06 04:28:13 +0000

    exept letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if ((alpha == 'q') || (alpha == 'e'))
+			continue;
+		else
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
