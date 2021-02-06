12808189575018973d0d82456f9904cbaf944b55
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-06 02:35:37 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
@@ -8,7 +6,7 @@
  */
 int main(void)
 {
-	printf("Sixe of chart: %d byte(s)\n", sizeof(char));
+	printf("Sixe of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
