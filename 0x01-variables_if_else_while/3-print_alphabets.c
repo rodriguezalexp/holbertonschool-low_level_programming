b7222c4d2a13d4688fc607e5fe91e8278f834618
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-06 04:17:15 +0000

    alphabet- ALPHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char z, Z;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
