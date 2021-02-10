ff3a000a5a07ee0b4425bf5c0b461fefba9b5ade
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-10 19:08:05 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - print absolute value
+ * @a: a
+ * Return: return a
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
