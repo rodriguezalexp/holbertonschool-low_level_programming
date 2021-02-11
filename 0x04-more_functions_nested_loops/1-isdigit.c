52a9ea7160fb912049c1370554b47c9b6cc0451a
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-11 17:46:07 +0000

    checks digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+/**
+ * _isdigit - checks for a digit
+ * @c: is the argument of the protoype
+ * Return: 1 if is c, 0 oyherwise
+ */
+
+int _isdigit(int c)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = i;
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
