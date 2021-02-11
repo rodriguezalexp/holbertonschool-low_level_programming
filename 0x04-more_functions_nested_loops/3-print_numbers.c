89006660656c67deb8c662b7ee3b1c1e1080bd87
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-11 19:49:25 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - checks for a digit
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+
+{
+	char c;
+
+	for (c = '0' ; c <= '9'; c++)
+
+		_putchar(c);
+
+	_putchar('\n');
+
+}
