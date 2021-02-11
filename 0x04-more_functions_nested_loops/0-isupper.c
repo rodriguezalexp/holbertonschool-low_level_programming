17e308c479a70e36ed7cd034cdce7f66d2a8d57c
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-11 16:53:45 +0000

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - checks if passes character is uppercase
+ * @c: is the argument of the protoype
+ * Return: 1 if is c, 0 oyherwise
+ */
+
+int _isupper(int c)
+
+{
+if (c >= 'A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More funtions nested in c.
