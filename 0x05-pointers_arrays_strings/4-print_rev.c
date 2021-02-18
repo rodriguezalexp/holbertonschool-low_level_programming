9fce63dbced1bfde18fe207f9634eba0a8520d41
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-18 17:49:29 +0000

    reverse string 1.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= '\0')
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
