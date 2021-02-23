a5e34d7cab08903338eb1c08227d881c82be843c
rodriguezalexp <alexander.0894-rp@hotmail.com>
2021-02-23 17:29:54 +0000

    last half of array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,16 @@
  */
 void puts_half(char *str)
 {
-
-	int i = 0;
+	int i;
+	int b;
+        i = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
-		if (i >= 5)
+		b = i / 2;
+		if (i % 2)
+			b++;
 			_putchar (str[i]);
 	}
 
