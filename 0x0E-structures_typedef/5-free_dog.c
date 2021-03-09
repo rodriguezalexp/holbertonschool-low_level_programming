f7bc538d6671454d32827920a2a261e6b522c9ec
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-09 03:18:55 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog
+ * @d: pointer to structure
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
