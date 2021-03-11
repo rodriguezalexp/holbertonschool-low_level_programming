4d86ba1d1d2bff56b5c23866802e89f405b36a6f
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-11 05:10:09 +0000

    pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-* print_name - print name
-* @name: name to print
-* @f: funtion to print
-*
-* Return: void
-*/
+ * print_name - print name
+ * @name: name to print
+ * @f: funtion to print
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
