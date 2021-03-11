7e37b074b837852dd85ae4079ba0bc9162a79f9d
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-11 23:12:57 +0000

    sum them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,18 @@
 #define _VARIADIC_FUNTIONS_H_
 
 #include <stdarg.h>
+
+typedef struct print
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 
