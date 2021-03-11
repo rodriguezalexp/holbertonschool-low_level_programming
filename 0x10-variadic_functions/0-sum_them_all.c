7c148ea10ed064af09f32059bbdb05778210cdc3
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-11 23:00:18 +0000

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,6 +16,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(parameters, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 		s += va_arg(parameters, int);
 	va_end(parameters);
