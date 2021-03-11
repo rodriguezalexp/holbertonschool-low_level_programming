3016bfd39f62510c0161b5f30dd8883056c26fd3
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-11 05:07:05 +0000

    pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
-* print_name - funtion to a pointer
+* print_name - print name
 * @name: name to print
 * @f: funtion to print
 *
-* return: void
+* Return: void
 */
 void print_name(char *name, void (*f)(char *))
 {
