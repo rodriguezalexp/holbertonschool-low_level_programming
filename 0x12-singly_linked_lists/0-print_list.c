a8fa35967d6d2eacdc90b83de3795de6904d841b
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-03-24 02:02:52 +0000

    print nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - prints all the elements of a list_t list
+* @h: pointer to the start of the list
+*
+* Return: number of nodes in the list
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+	unsigned int i;
+
+	temp = h;
+	for (i = 0; temp; i++)
+	{
+		printf("[%u] %s\n", temp->len, temp->str);
+		temp = temp->next;
+	}
+	return (i);
+}
