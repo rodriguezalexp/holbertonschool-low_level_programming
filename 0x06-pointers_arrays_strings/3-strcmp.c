9dc45f473f32764f5a19ef6b1c1e6fef78d3fd3b
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-04-05 03:51:23 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,18 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (*(s1 + i) && *(s2 + i))
+	while (*(s1 + i) && *(s2 + i) && *(s1 + i) == *(s2 + i))
 	{
 		i++;
 	}
-
-	if (*(s1 + i) == (*s2 + i))
-	{
-		return (0);
-	}
-
-	else
-	{
-		return (*(s1 + i) - *(s2 + i));
-	}
+	return (*(s1 + i) - *(s2 + i));
 }
