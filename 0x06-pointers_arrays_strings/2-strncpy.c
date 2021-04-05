b73096bac2df8f7aa3c558ed97fd7929da54cb70
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-04-05 02:27:28 +0000

    copy second string to firt string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strncpy - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: number of bytes from src
+ * Return: address of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	if (i < n)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
