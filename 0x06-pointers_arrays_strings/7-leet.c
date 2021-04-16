dd36653e82ed13c82b00b06bdcff80910125467c
Alexander Rodriguez <alexander.0894-rp@hotmail.com>
2021-04-16 00:30:01 +0000

    add

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,16 @@
  */
 char *leet(char *s)
 {
-	int count, j;
+	int i, j;
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; *(s + count); count++)
+	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (a[j] == *(s + count))
-				*(s + count) = b[j];
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
 	}
 	return (s);
